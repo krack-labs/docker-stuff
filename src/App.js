2e25c6121523abecbee9bebb3efe9dd1d8df3664
root <sutcliffe.andrew@yahoo.co.uk>
2020-09-06 18:57:17 +0100

    last change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> time to go.
         </p>
         <a
           className="App-link"
@@ -16,8 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         learn react
-	  I was changed on the feature branch
+          Learn React
         </a>
       </header>
     </div>
